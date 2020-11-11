c5e565ecbd325943f57ce2efb393e7fc83b86eba
khu23 <you@example.com>
2020-11-11 15:08:03 +0530

    update

--- src/screens/loginScreen/LoginScreen.js
+++ src/screens/loginScreen/LoginScreen.js
@@ -1,82 +1,50 @@
 
-import React, { Component, useState } from 'react'
+import React, { Component } from 'react'
 import { ImageBackground, View, StyleSheet, FlatList, Text, TextInput, TouchableOpacity, ScrollView } from 'react-native'
-import { emailValidator, passwordValidator } from '../../components/core/utils'
-// export default class Login extends Component {
-//     state = {
-//         email: "",
-//         password: ""
-//     }
-const LoginScreen = ({ navigation }) => {
-    const [email, setEmail] = useState({ value: '', error: '' });
-    const [password, setPassword] = useState({ value: '', error: '' });
 
-    const _onLoginPressed = () => {
-        const emailError = emailValidator(email.value);
-        const passwordError = passwordValidator(password.value);
+export default class LoginScreen extends Component {
+    state = {
+        email: "",
+        password: ""
+    }
 
-        if (emailError || passwordError) {
-            setEmail({ ...email, error: emailError });
-            setPassword({ ...password, error: passwordError });
-            return;
-        }
+    render() {
 
-        // navigation.navigate('Home');
-    };
-    // render() {
-    console.log(email.value);
-    console.log(password.value);
-    return (
-        <ImageBackground source={require('../../../assets/images/background.png')} style={styles.backgroundImage}>
-            <View style={styles.inputView}>
-                <TextInput
-                    style={styles.inputText}
-                    placeholder="Email"
-                    // placeholderTextColor="#000000"
-                    // onChangeText={text => this.setState({ email: text })}
-                    label="Email"
-                    returnKeyType="next"
-                    value={email.value}
-                    onChangeText={text => setEmail({ value: text, error: '' })}
-                    error={!!email.error}
-                    errorText={email.error}
-                    autoCapitalize="none"
-                    autoCompleteType="email"
-                    textContentType="emailAddress"
-                    keyboardType="email-address"
-                />
+        return (
+            <ImageBackground source={require('../../../assets/images/background.png')} style={styles.backgroundImage} >
+                <View style={styles.inputView}>
+                    <TextInput
+                        style={styles.inputText}
+                        placeholder="Email"
+                        placeholderTextColor="#000000"
+                        onChangeText={text => this.setState({ email: text })}
 
-            </View>
-            <View style={styles.inputView} >
-                <TextInput
-                    secureTextEntry
-                    style={styles.inputText}
-                    placeholder="Password"
-                    // placeholderTextColor="#000000"
-                    // onChangeText={text => this.setState({ password: text })}
-                    label="Password"
-                    returnKeyType="done"
-                    value={password.value}
-                    onChangeText={text => setPassword({ value: text, error: '' })}
-                    error={!!password.error}
-                    errorText={password.error}
-                    secureTextEntry
+                    />
 
-                />
-            </View>
-            <TouchableOpacity onPress={() => this.props.navigation.navigate('ForgotPassword')}>
-                <Text style={styles.forgot}>Forgot Password?</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.loginBtn} mode="contained" onPress={_onLoginPressed}>
-                <Text style={styles.loginText} >LOGIN</Text>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={() => this.props.navigation.navigate('Register')} >
-                <Text style={styles.signupText} >Signup</Text>
-            </TouchableOpacity>
-        </ImageBackground>
-    )
+                </View>
+                <View style={styles.inputView} >
+                    <TextInput
+                        secureTextEntry
+                        style={styles.inputText}
+                        placeholder="Password"
+                        placeholderTextColor="#000000"
+                        onChangeText={text => this.setState({ password: text })}
+
+                    />
+                </View>
+                <TouchableOpacity onPress={() => this.props.navigation.navigate('ForgotPassword')}>
+                    <Text style={styles.forgot}>Forgot Password?</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.loginBtn} >
+                    <Text style={styles.loginText} >LOGIN</Text>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => this.props.navigation.navigate('Register')} >
+                    <Text style={styles.signupText} >Signup</Text>
+                </TouchableOpacity>
+            </ImageBackground>
+        )
+    }
 }
-// }
 
 const styles = StyleSheet.create({
     backgroundImage: {
@@ -145,4 +113,3 @@ const styles = StyleSheet.create({
     // },
 });
 
-export default LoginScreen
