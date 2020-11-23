d9df3c6f05fb981ad61733d9ed1b3bc3ee1aba7c
khu23 <you@example.com>
2020-11-23 16:05:09 +0530

    update

--- src/screens/homeScreen/HomeScreen.js
+++ src/screens/homeScreen/HomeScreen.js
@@ -2,85 +2,93 @@ import React, { Component } from 'react'
 import { View, StyleSheet, Text, Image, TouchableOpacity, ScrollView, SafeAreaView } from 'react-native'
 import SliderScreen from '../../components/Slider/SliderScreen'
 import { MaterialIcons } from '@expo/vector-icons';
-// import img from '../../../assets/images/coffee.png'
-// import { color } from 'react-native-reanimated';
-import BottomNavigator from '../../navigations/BottomNavigation'
+import { Card } from 'react-native-elements'
+
 
 export default class Home extends Component {
     render() {
         return (
-            <View>
-                <ScrollView>
-                    <View style={styles.design}>
-                        {/* <Image style={styles.image}
-                            source={require('../../../assets/images/logo.png')}
-                        />
-                         */}
-                        <SliderScreen />
-                        <Text style={styles.textview}>Recipes By Caregories</Text>
-                        <SafeAreaView style={{ flex: 1, marginTop: 10, marginLeft: 10 }}>
+            <View style={styles.continer}>
+
+                <View style={styles.design}>
+
+                    <SliderScreen />
+                    <Text style={styles.textview}>Recipes By Caregories</Text>
+                    <View style={styles.cardstyle}>
+                        <Card style={styles.cardlayout}>
                             <TouchableOpacity style={styles.button}>
                                 <MaterialIcons
                                     name={'free-breakfast'}
                                     size={30}
+                                    style={styles.icon}
                                 />
-                                <Text style={styles.buttontext}>BreakFast</Text>
-                                {/* <Text style={styles.buttontext}>24 Recipes</Text> */}
+                                <Text style={styles.buttontext}>BreakFast &nbsp;&nbsp; <Text style={styles.innerText}>24 Recipes</Text>
+                                </Text>
                             </TouchableOpacity>
-                        </SafeAreaView>
-                        <SafeAreaView style={{ flex: 1, marginTop: 10, marginLeft: 10 }}>
+                        </Card>
+                        <Card >
+
                             <TouchableOpacity style={styles.button}>
                                 <MaterialIcons
                                     name={'free-breakfast'}
                                     size={30}
+                                    style={styles.icon}
                                 />
-                                <Text style={styles.buttontext}>BreakFast</Text>
-                                {/* <Text style={styles.buttontext}>24 Recipes</Text> */}
+                                <Text style={styles.buttontext}>Lunch &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  <Text style={styles.innerText}>106 Recipes</Text>
+                                </Text>
+
                             </TouchableOpacity>
-                        </SafeAreaView>
-                        <SafeAreaView style={{ flex: 1, marginTop: 10, marginLeft: 10 }}>
+
+                        </Card>
+                        <Card >
+
                             <TouchableOpacity style={styles.button}>
                                 <MaterialIcons
                                     name={'free-breakfast'}
                                     size={30}
+                                    style={styles.icon}
                                 />
-                                <Text style={styles.buttontext}>BreakFast</Text>
-                                {/* <Text style={styles.buttontext}>24 Recipes</Text> */}
+                                <Text style={styles.buttontext}>Dinner &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  <Text style={styles.innerText}>24 Recipes</Text>
+                                </Text>
+
                             </TouchableOpacity>
-                        </SafeAreaView>
+
+                        </Card>
+
+
+
                     </View>
-                    <BottomNavigator />
-                </ScrollView>
+                </View>
+
+
             </View>
         )
     }
 }
 
 const styles = StyleSheet.create({
+    continer: {
+        flex: 1,
+    },
     button: {
         alignItems: 'center',
         justifyContent: 'center',
-        flex: 1,
-        borderRadius: 5,
         flexDirection: 'row',
-        width: 140,
-        height: 70,
-        padding: 10,
-        borderWidth: 2,
-        borderColor: '#F67600',
-        borderTopLeftRadius: 1,
-        borderStyle: 'solid'
+        width: 120,
+        height: 50,
     },
     design: {
         marginTop: 24,
     },
     image: {
         height: '30%',
-        width: '50%'
+        width: '50%',
+
     },
     icon: {
-        height: '40%',
-        width: '40%',
+        // height: '40%',
+        // width: '40%',
+        marginLeft: 10
     },
     textview: {
         marginTop: 10,
@@ -89,7 +97,19 @@ const styles = StyleSheet.create({
         fontWeight: 'bold'
     },
     buttontext: {
-        marginLeft: 5,
+        marginLeft: 10,
         fontSize: 20,
-    }
+
+    },
+    cardstyle: {
+        alignItems: 'center',
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+
+    },
+    innerText: {
+        fontSize: 12,
+        alignItems: 'center',
+    },
+
 });
